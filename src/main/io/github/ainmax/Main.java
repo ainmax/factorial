e05b9db915e0a6223f243fddbf479b809c919ffc
rmakhmutov <rmakhmutov@i-novus.ru>
2020-05-11 14:41:19 +0300

    cf performance benchmark

--- src/main/io/github/ainmax/Main.java
+++ src/main/io/github/ainmax/Main.java
@@ -3,6 +3,9 @@ package io.github.ainmax;
 public class Main {
 
     public static void main(String[] args) {
+        long before = System.currentTimeMillis();
         System.out.println(Factorial.calculate(Long.parseLong(args[0])));
+        long now = System.currentTimeMillis();
+        System.out.println("Seconds elapsed: " + (now-before)/1000F + " seconds." );
     }
 }
